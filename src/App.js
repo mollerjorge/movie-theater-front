f703c459895d901e9e25849f2150086804316102
mollerjorge <jmoller2106@gmail.com>
2020-02-06 20:05:27 -0300

    Add react-intl

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { IntlProvider, useIntl } from 'react-intl'
+import flatten from 'flat'
+import AppLocale from './languageProvider'
 
-function App() {
-  return <div className="App">This is a boiler plate</div>
+const App = () => {
+  const currentLocale = navigator.language
+  const currentAppLocale = AppLocale[currentLocale]
+
+  return (
+    <IntlProvider
+      locale={currentLocale}
+      messages={flatten(currentAppLocale.messages)}
+    >
+      <SearchBar />
+    </IntlProvider>
+  )
+}
+
+const SearchBar = () => {
+  const intl = useIntl()
+  const searchBarPlaceholder = intl.formatMessage({
+    id: 'home.searchBarPlaceholder'
+  })
+
+  return <input placeholder={searchBarPlaceholder} />
 }
 
 export default App
